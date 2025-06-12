538eaef6f8e1042a830140eda197224a1a06b732
Frédéric Gobfert <frederic.gobfert@gmail.com>
2025-06-12 10:00:03 +0200

    résolution pb affichage icones

--- App1/constantes.py
+++ App1/constantes.py
@@ -4,8 +4,8 @@ from PyQt6.QtCore import Qt
 
 class Constantes:
     TAILLE_CASE : int = 100
-    CHEMIN_IMAGE_PLAN : str = sys.path[0] + '/../img/plan.jpg'
+    CHEMIN_IMAGE_PLAN : str = sys.path[0] + './img/plan.jpg'
     EPAISS_LIGNE_QUADRILLAGE : int = 3
     COULEUR_QUADRILLAGE = Qt.GlobalColor.red
-    CHEMIN_ICONES : str = sys.path[0] + '/../img/icones/'
+    CHEMIN_ICONES : str = sys.path[0] + './img/icones/'
     
